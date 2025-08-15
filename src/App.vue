756418d1f2ff38f02215a94db7c2e89105037ad4
mashiyu20020410 <dj8817117@gmail.com>
2025-08-16 01:53:38 +0800

    生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup>
 // 导入组件
-import MyComponent01 from './components/MyComponent01.vue'
-import MyComponent02 from './components/MyComponent02.vue'
-import MyComponent03 from './components/MyComponent03.vue'
-import MyComponent04 from './components/MyComponent04.vue'
-import MyComponent05 from './components/MyComponent05.vue'
-import MyComponent06 from './components/MyComponent06.vue'
-import MyComponent07 from './components/MyComponent07.vue'
-import MyComponent08 from './components/MyComponent08.vue'
-import MyComponent09 from './components/MyComponent09.vue'
-import MyComponent10 from './components/MyComponent10.vue'
+import MyComponent01 from './components/basis/MyComponent01.vue'
+import MyComponent02 from './components/basis/MyComponent02.vue'
+import MyComponent03 from './components/basis/MyComponent03.vue'
+import MyComponent04 from './components/basis/MyComponent04.vue'
+import MyComponent05 from './components/basis/MyComponent05.vue'
+import MyComponent06 from './components/basis/MyComponent06.vue'
+import MyComponent07 from './components/basis/MyComponent07.vue'
+import MyComponent08 from './components/basis/MyComponent08.vue'
+import MyComponent09 from './components/basis/MyComponent09.vue'
+import MyComponent10 from './components/basis/MyComponent10.vue'
+import MyComponent11 from './components/basis/MyComponent11.vue'
 
 </script>
 
 <template>
+
+  <!-- --------------------------------------------------基础---------------------------------------------------- -->
   <!-- 1.ref的使用 -->
   <!-- <MyComponent01 /> -->
 
@@ -42,7 +45,15 @@ import MyComponent10 from './components/MyComponent10.vue'
   <!-- <MyComponent09 /> -->
 
   <!-- 10.组件基础 -->
-  <MyComponent10 />
+  <!-- <MyComponent10 /> -->
+
+  <!-- 11.生命周期钩子 -->
+  <!-- <MyComponent11 /> -->
+
+  <!-- --------------------------------------------------深入组件---------------------------------------------------- -->
+
+
+
 
 </template>
 
---
+++ src/components/basis/MyComponent11.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="my-component">
+
+    </div>
+</template>
+
+<script setup>
+// 引入生命周期钩子函数
+import { onMounted } from 'vue'
+
+// 执行在挂载后的自定义逻辑
+onMounted(() => {
+    alert('MyComponent11 组件已挂载')
+})
+
+</script>
+
+<style scoped></style>
